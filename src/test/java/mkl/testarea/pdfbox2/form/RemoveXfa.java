3210b3ebfedabbc32ddd37501a1f29d7d3fe5fd2
mkl <mkl@wir-sind-cool.org>
2020-11-25 10:35:09 +0100

    New RemoveXfa tests focusing on the PDFBox issue PDFBOX-5012
    Signature Field Not Recognize
    https://issues.apache.org/jira/browse/PDFBOX-5012
    PDF_orig_anon.pdf
    https://issues.apache.org/jira/secure/attachment/13014820/PDF_orig_anon.pdf
    
    testRemoveXfaAndUr3FromPDF_orig_anon - This code (essentially by Maruan Sahyoun) demonstrates how to correctly remove XFA and UR3 from the OP's original example file.

---
+++ src/test/java/mkl/testarea/pdfbox2/form/RemoveXfa.java
@@ -0,0 +1,62 @@
+package mkl.testarea.pdfbox2.form;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author mkl
+ */
+public class RemoveXfa {
+    final static File RESULT_FOLDER = new File("target/test-outputs", "form");
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        RESULT_FOLDER.mkdirs();
+    }
+
+    /**
+     * <a href="https://issues.apache.org/jira/browse/PDFBOX-5012">
+     * Signature Field Not Recognize
+     * </a>
+     * <br/>
+     * <a href="https://issues.apache.org/jira/secure/attachment/13014820/PDF_orig_anon.pdf">
+     * PDF_orig_anon.pdf
+     * </a>
+     * <p>
+     * This code (essentially by Maruan Sahyoun) demonstrates how to
+     * correctly remove XFA and UR3 from the OP's original example file.
+     * </p>
+     */
+    @Test
+    public void testRemoveXfaAndUr3FromPDF_orig_anon() throws IOException {
+        try (   InputStream resource = getClass().getResourceAsStream("PDF_orig_anon.pdf")  ) {
+            PDDocument document = Loader.loadPDF(resource);
+
+            PDDocumentCatalog documentCatalog = document.getDocumentCatalog();
+            PDAcroForm acroForm = documentCatalog.getAcroForm();
+            assertTrue(acroForm.hasXFA());
+            assertFalse(acroForm.xfaIsDynamic());
+            acroForm.getCOSObject().removeItem(COSName.XFA);
+            assertFalse(acroForm.hasXFA());
+
+            // Remove U3 usage signature
+            COSDictionary permsDict = documentCatalog.getCOSObject().getCOSDictionary(COSName.PERMS);
+            permsDict.removeItem(COSName.getPDFName("UR3"));
+
+            document.save(new File(RESULT_FOLDER, "PDF_orig_anon-noXfaUr3.pdf"));
+        }
+    }
+
+}
