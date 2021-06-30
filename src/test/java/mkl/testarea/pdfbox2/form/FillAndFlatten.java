6ed57cadf5adb7cfa729fb0c8614362fc9925d3d
mkl <klink@csi.com>
2021-06-30 10:36:55 +0200

    New FillAndFlatten test focusing on the stack overflow question 68190255
    PDFBox field value cannot display after flatten
    https://stackoverflow.com/questions/68190255/pdfbox-field-value-cannot-display-after-flatten
    test.pdf as "testVinciChung.pdf"
    https://drive.google.com/file/d/1VPIMe3UltcDcmHDpyVs_2eMtXDkiSHhU/view?usp=sharing
    
    testLikeVinciChung - Works as expected. The OP should simply update his PDFBox dependency.

--- src/test/java/mkl/testarea/pdfbox2/form/FillAndFlatten.java
+++ src/test/java/mkl/testarea/pdfbox2/form/FillAndFlatten.java
@@ -95,4 +95,54 @@ public class FillAndFlatten {
             pdDocument.save(new File(RESULT_FOLDER, "b3-3-remove-action-filled-and-flattened.pdf"));
         }
     }
+
+    /**
+     * <a href="https://stackoverflow.com/questions/68190255/pdfbox-field-value-cannot-display-after-flatten">
+     * PDFBox field value cannot display after flatten
+     * </a>
+     * <br/>
+     * <a href="https://drive.google.com/file/d/1VPIMe3UltcDcmHDpyVs_2eMtXDkiSHhU/view?usp=sharing">
+     * test.pdf
+     * </a> as "testVinciChung.pdf"
+     * <p>
+     * Works as expected. The OP should simply update his PDFBox dependency.
+     * </p>
+     */
+    @Test
+    public void testLikeVinciChung() throws IOException {
+        try (   InputStream resource = getClass().getResourceAsStream("testVinciChung.pdf");
+                PDDocument pdDocument = Loader.loadPDF(resource)    ) {
+            PDAcroForm acroForm = pdDocument.getDocumentCatalog().getAcroForm();
+
+            String defaultAppearance = "/Helv 12 Tf 0 0 1 rg";
+
+            for(PDField field : acroForm.getFields()) {
+                if (field instanceof PDTextField) {
+                    PDTextField textField = (PDTextField) field;
+                    textField.setActions(null);             
+                    textField.setDefaultAppearance(defaultAppearance);
+                }
+            }
+
+            setFormValue(acroForm, "TextBox", "testvalue");
+            setFormValue(acroForm, "radioBtn", "Yes");
+            setFormValue(acroForm, "chkBox", "Yes");
+            acroForm.refreshAppearances();
+            acroForm.flatten();
+
+            pdDocument.save(new File(RESULT_FOLDER, "testVinciChung-filled-and-flattened.pdf"));
+        }
+    }
+
+    /**
+     * @see #testLikeVinciChung()
+     */
+    private static void setFormValue(PDAcroForm acroForm, String key, String value) throws IOException {
+        PDField f = acroForm.getField(key);
+        if (f != null) {
+            if (value != null && value.trim().length() > 0) {
+                f.setValue(value);
+            }
+        }
+    }
 }
