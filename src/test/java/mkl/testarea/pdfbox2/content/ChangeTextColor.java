6cc971c5ecb42a155b54bf5262654677771cb85a
mkl <klink@csi.com>
2021-07-13 17:56:56 +0200

    New ChangeTextColor test focusing on the stack overflow question 68299177
    PDFBox - Text displayed in blue although PDAnnotation removed
    https://stackoverflow.com/questions/68299177/pdfbox-text-displayed-in-blue-although-pdannotation-removed
    Test2.pdf as "Test2-SureshbabuKatta.pdf"
    https://github.com/sureshbabukatta/pdfbox-remove-annotation
    
    testRemoveSomeAnnotationsAndMakeTextBlackThereTest2 - In addition to removing the annotations one has to edit the static page content. This test method shows how to do so in the areas of the removed annotations only.

--- src/test/java/mkl/testarea/pdfbox2/content/ChangeTextColor.java
+++ src/test/java/mkl/testarea/pdfbox2/content/ChangeTextColor.java
@@ -15,11 +15,14 @@ import org.apache.pdfbox.cos.COSInteger;
 import org.apache.pdfbox.pdfwriter.ContentStreamWriter;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.graphics.color.PDColor;
 import org.apache.pdfbox.pdmodel.graphics.color.PDColorSpace;
 import org.apache.pdfbox.pdmodel.graphics.color.PDDeviceCMYK;
 import org.apache.pdfbox.pdmodel.graphics.color.PDDeviceGray;
 import org.apache.pdfbox.pdmodel.graphics.color.PDDeviceRGB;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
+import org.apache.pdfbox.util.Matrix;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -101,6 +104,10 @@ public class ChangeTextColor {
         }
     }
 
+    /**
+     * @see #testMakeTextBlackTestAfterRemovingAnnotation()
+     * @see #testRemoveSomeAnnotationsAndMakeTextBlackThereTest2()
+     */
     static boolean isBlack(PDColor pdColor) {
         PDColorSpace pdColorSpace = pdColor.getColorSpace();
         float[] components = pdColor.getComponents();
@@ -112,5 +119,107 @@ public class ChangeTextColor {
             return components[0] < .1f && components[1] < .1f && components[2] < .1f;
         else
             return false;
+    }
+
+    /**
+     * <a href="https://stackoverflow.com/questions/68299177/pdfbox-text-displayed-in-blue-although-pdannotation-removed">
+     * PDFBox - Text displayed in blue although PDAnnotation removed
+     * </a>
+     * <br/>
+     * <a href="https://github.com/sureshbabukatta/pdfbox-remove-annotation">
+     * Test2.pdf
+     * </a> as "Test2-SureshbabuKatta.pdf"
+     * <p>
+     * In addition to removing the annotations one has to edit the static page content.
+     * This test method shows how to do so in the areas of the removed annotations only.
+     * </p>
+     */
+    @Test
+    public void testRemoveSomeAnnotationsAndMakeTextBlackThereTest2() throws IOException {
+        try (   InputStream resource = getClass().getResourceAsStream("Test2-SureshbabuKatta.pdf");
+                PDDocument document = Loader.loadPDF(resource)) {
+            for (PDPage page : document.getDocumentCatalog().getPages()) {
+                List<PDRectangle> areas = new ArrayList<>();
+                // Remove every other annotation, collect their areas
+                List<PDAnnotation> annotations = new ArrayList<>();
+                boolean remove = true;
+                for (PDAnnotation annotation : page.getAnnotations()) {
+                    if (remove)
+                        areas.add(annotation.getRectangle());
+                    else
+                        annotations.add(annotation);
+                    remove = !remove;
+                }
+                page.setAnnotations(annotations);
+
+                PdfContentStreamEditor editor = new PdfContentStreamEditor(document, page) {
+                    @Override
+                    protected void write(ContentStreamWriter contentStreamWriter, Operator operator, List<COSBase> operands) throws IOException {
+                        String operatorString = operator.getName();
+
+                        if (TEXT_SHOWING_OPERATORS.contains(operatorString) && isInAreas()) {
+                            if (currentlyReplacedColor == null)
+                            {
+                                PDColor currentFillColor = getGraphicsState().getNonStrokingColor();
+                                if (!isBlack(currentFillColor))
+                                {
+                                    currentlyReplacedColor = currentFillColor;
+                                    super.write(contentStreamWriter, SET_NON_STROKING_GRAY, GRAY_BLACK_VALUES);
+                                }
+                            }
+                        } else if (currentlyReplacedColor != null) {
+                            PDColorSpace replacedColorSpace = currentlyReplacedColor.getColorSpace();
+                            List<COSBase> replacedColorValues = new ArrayList<>();
+                            for (float f : currentlyReplacedColor.getComponents())
+                                replacedColorValues.add(new COSFloat(f));
+                            if (replacedColorSpace instanceof PDDeviceCMYK)
+                                super.write(contentStreamWriter, SET_NON_STROKING_CMYK, replacedColorValues);
+                            else if (replacedColorSpace instanceof PDDeviceGray)
+                                super.write(contentStreamWriter, SET_NON_STROKING_GRAY, replacedColorValues);
+                            else if (replacedColorSpace instanceof PDDeviceRGB)
+                                super.write(contentStreamWriter, SET_NON_STROKING_RGB, replacedColorValues);
+                            else {
+                                //TODO
+                            }
+                            currentlyReplacedColor = null;
+                        }
+
+                        super.write(contentStreamWriter, operator, operands);
+
+                        before = null;
+                        after = null;
+                    }
+
+                    PDColor currentlyReplacedColor = null;
+
+                    final List<String> TEXT_SHOWING_OPERATORS = Arrays.asList("Tj", "'", "\"", "TJ");
+                    final Operator SET_NON_STROKING_CMYK = Operator.getOperator("k");
+                    final Operator SET_NON_STROKING_RGB = Operator.getOperator("rg");
+                    final Operator SET_NON_STROKING_GRAY = Operator.getOperator("g");
+                    final List<COSBase> GRAY_BLACK_VALUES = Arrays.asList(COSInteger.ZERO);
+
+                    @Override
+                    protected void showText(byte[] string) throws IOException {
+                        Matrix ctm = getGraphicsState().getCurrentTransformationMatrix();
+                        if (before == null)
+                            before = getTextMatrix().multiply(ctm);
+                        super.showText(string);
+                        after = getTextMatrix().multiply(ctm);
+                    }
+
+                    Matrix before = null;
+                    Matrix after = null;
+
+                    boolean isInAreas() {
+                        return isInAreas(before) || isInAreas(after);
+                    }
+                    boolean isInAreas(Matrix m) {
+                        return m != null && areas.stream().anyMatch(rect -> rect.contains(m.getTranslateX(), m.getTranslateY()));
+                    }
+                };
+                editor.processPage(page);
+            }
+            document.save(new File(RESULT_FOLDER, "Test2-withoutSomeAnnotation-withBlackTextThere.pdf"));
+        }
     }
 }
